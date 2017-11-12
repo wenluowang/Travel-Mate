74c90760952a7a193cbe36d26dbb9f868ea2189e
wenluowang <wangwenluo@yeah.net>
2017-11-12 11:50:07 -0600

    commit database change

--- APIs/inc/connection.inc.php
+++ APIs/inc/connection.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 $connect_error = 'Could not connect';
-$mysql_host = 'XXXX';
-$mysql_user = 'XXXX';
-$mysql_pass = 'XXXX';
-$mysql_data = 'XXXX';
+$mysql_host = 'ec2-184-73-174-10.compute-1.amazonaws.com';
+$mysql_user = 'sgmxjreazdlahd';
+$mysql_pass = '559213071084f9efd5b3cc7266d5f56bd692374e682f6d3efc2056c8264d3abd';
+$mysql_data = 'de1kl6ikd8g8ne';
 
 
 function get_mysql_connection() {
